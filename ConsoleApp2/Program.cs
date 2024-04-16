7d098097f09ac9596ef01be5b350ce9d1ddd9f2f
mee1n <mee1n@DESKTOP-D7UO30K>
2024-04-16 22:43:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Load sample data
+using ConsoleApp2;
+
+string text =Console.ReadLine();
+
+var sampleData = new MLModel1.ModelInput()
+{
+    Text = @"Oh dear an evening of absolute hilarity I don't think I have laughed so much in a long time! 😂",
+};
+sampleData.Text = text;
+
+//Load model and predict output
+var result = MLModel1.Predict(sampleData);
+var mood = result.PredictedLabel;
+Console.WriteLine($"Предположительное настроение: {mood} ");
